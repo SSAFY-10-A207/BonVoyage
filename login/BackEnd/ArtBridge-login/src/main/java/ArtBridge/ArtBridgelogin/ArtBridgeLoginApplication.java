a675df7169f692784cc67a5664f01dd2f01aa248
lynne921 <dkfls921@gmail.com>
2024-01-24 14:04:13 +0900

    feat: Initial backend(spring environment) setup

---
+++ login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/ArtBridgeLoginApplication.java
@@ -0,0 +1,13 @@
+package ArtBridge.ArtBridgelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtBridgeLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtBridgeLoginApplication.class, args);
+	}
+
+}
---
+++ login/BackEnd/ArtBridge-login/src/test/java/ArtBridge/ArtBridgelogin/ArtBridgeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package ArtBridge.ArtBridgelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtBridgeLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
